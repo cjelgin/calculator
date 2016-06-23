47a4ff68bdd4f2fab97d11416ef3068e3053546b
Carl Elgin <cjelgin@umich.edu>
2016-06-23 15:20:44 -0400

    Made more progress on the calc actions

--- app/src/main/java/com/example/carl/calculator/MainActivity.java
+++ app/src/main/java/com/example/carl/calculator/MainActivity.java
@@ -3,16 +3,101 @@ package com.example.carl.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    LinearLayout background;
+    Button one, two, three, plus, equals, clear;
+    TextView calcDisplay;
+
     private final String TAG = "CLC";
+    public String curInput;
+    public Integer sumInput=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calc_layout);
         Log.d(TAG,"onCreate");
+
+
+        background=(LinearLayout) findViewById(R.id.background);
+        calcDisplay=(TextView) findViewById(R.id.calcDisplay);
+        one=(Button) findViewById(R.id.one);
+        two=(Button) findViewById(R.id.two);
+        three=(Button) findViewById(R.id.three);
+        plus=(Button) findViewById(R.id.plus);
+        equals=(Button) findViewById(R.id.equals);
+        clear=(Button) findViewById(R.id.clear);
+
+
+        //Action for number 1
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Action for number 2
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Action for number 3
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Action for plus
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //Action for equals
+        equals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0;i<curInput.length();i++){
+                    if(curInput[i].isInteger){
+                        sumInput=sumInput+curInput[i];
+                    }
+
+                }
+                calcDisplay.setText(curInput);
+            }
+        });
+
+        //Action for clear
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curInput="0";
+
+            }
+        });
+
+
+
+
+
+
+
     }
 
 
